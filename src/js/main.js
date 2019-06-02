99fc725f78ab00c272a18b7c3dcdaa8ae4733602
atallah-salah <atallah_salah@outlook.com>
2019-06-02 03:14:18 +0300

    Build UI

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var elems = document.querySelectorAll(".sidenav");
-  var instances = M.Sidenav.init(elems, { edge: "left" });
+  // nav menu
+  const forms = document.querySelectorAll(".side-menu");
+  M.Sidenav.init(forms, { edge: "left" });
+
+  document.querySelector("#menu").addEventListener("click", () => {
+    document.querySelector("#card-quote").classList.remove("scale-out");
+    document.querySelector("#card-content-quote").classList.toggle("scale-out");
+  });
 });
