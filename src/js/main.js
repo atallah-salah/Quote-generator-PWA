84998db0470fd5d13d4791303bf42c54282e023f
atallah-salah <atallah_salah@outlook.com>
2019-06-02 14:46:33 +0300

    add dropdowm menu to the left nav

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,11 @@ document.addEventListener("DOMContentLoaded", function() {
   const forms = document.querySelectorAll(".side-menu");
   M.Sidenav.init(forms, { edge: "left" });
 
+  // var elems = document.querySelectorAll('.dropdown-trigger');
+  // var instances = M.Dropdown.init(elems);
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems);
+
   document.querySelector("#menu").addEventListener("click", () => {
     document.querySelector("#card-quote").classList.remove("scale-out");
     document.querySelector("#card-content-quote").classList.toggle("scale-out");
