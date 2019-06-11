6267a317bf35bbba807b7d400b08e2e01b2dd725
atallah-salah <atallah_salah@outlook.com>
2019-06-11 13:24:56 +0300

    add API for quote , Fix UI , add animation to qute content and generate button

--- src/js/getQuotes.js
+++ src/js/getQuotes.js
@@ -1,104 +1,34 @@
-var api = "https://en.wikiquote.org/w/api.php";
+const api='https://quote-api1.herokuapp.com/';
+let groupCount = localStorage.getItem('groupCount') || 0;
+let quteCount = localStorage.getItem('quteCount') || 0;
+let quotes = localStorage.getItem('quotes') && localStorage.getItem('quotes').split('//,') || getAllQuotes(groupCount);
 
-function getRandomStart(callback) {
-  $.ajax({
-    url: api,
-    data: {
-      "action": "query",
-      "format": "json",
-      "titles": "List of people by name",
-      "generator": "links",
-      "gplnamespace": "0",
-      "gpllimit": "20"
-    },
-    dataType: "jsonp",
-    success: function(jsondata) {
-      var links = jsondata.query.pages;
-      var pageIds = [];
-      for (var prop in links) {
-        pageIds.push(links[prop].pageid);
-      }
-      var rand = pageIds[Math.floor(Math.random() * pageIds.length)];
-      callback(rand);
-    },
-    error: function(xhr, callback) {
-      console.log("Error getting quotes");
-    }
-  });
-}
-
-function getRandomName(pageId, callback) {
-  $.ajax({
-    url: api + "?action=query&format=json&origin=*&prop=links&pageids=" + pageId + "&redirects=1&pllimit=max&callback=?",
-    dataType: "jsonp",
-    success: function(jsondata) {
-      var properId = Object.keys(jsondata.query.pages)[0];
-      var links = jsondata.query.pages[properId].links;
-      var randPerson = links[Math.floor(Math.random() * links.length)].title;
-      while (randPerson.indexOf("List of people") != -1) {
-        randPerson = links[Math.floor(Math.random() * links.length)].title;
-      }
-      currentAuthor = randPerson;
-      callback(randPerson);
-    },
-    error: function(xhr, callback) {
-      console.log("Error getting quotes");
-    }
-  });
-}
-
-function getRandomQuote(title, callback) {
-  $.ajax({
-    url: api,
-    data: {
-      "action": "parse",
-      "format": "json",
-      "origin": "*",
-      "page": title,
-      "prop": "text",
-      "section": "1",
-      "disablelimitreport": 1,
-      "disabletoc": 1
-    },
-    dataType: "jsonp",
-    success: function(jsondata) {
-      var text = jsondata.parse.text["*"];
-      var parser = new DOMParser();
-      var doc = parser.parseFromString(text, "text/html");
-      while (doc.querySelector("li > ul > li")) {
-        var toRemove = doc.querySelector("li > ul");
-        toRemove.parentNode.removeChild(toRemove);
-      }
-      var quotes = doc.querySelectorAll("ul > li");
-      var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-      currentQuote = randomQuote.textContent || randomQuote.innerText;
-      callback(randomQuote.textContent || randomQuote.innerText);
-    },
-    error: function(xhr, callback) {
-      console.log("Error getting quotes");
-    }
-  });
-}
-
-function renderQuote(callBackfunc) {
-  getRandomStart(function(c) {
-    getRandomName(c, function(author) {
-      getRandomQuote(author, function(quote) {
-        callBackfunc(quote,author)
-      });
-    });
-  });
-}
-
-function sendTweet(quote, author) {
-  var text = "";
-  if (quote.length + author.length + 1 < 140) {
-    text = "\"" + quote + "\" -" + author;
-    console.log(text);
-  } else {
-    var newLength = 140 - 45 - author.length - author.length;
-    var newquote = quote.replace("[\'\"]", "\\\"");
-    text = author + " said, \"" + newquote.substring(0, newLength) + "\"..." + "https://en.wikiquote.org/wiki/" + author.replace(" ", "_");
+const getQuote = ()=> {
+  if(quteCount>=100){
+    quteCount=0;
+    localStorage.setItem('quteCount',quteCount)
+    localStorage.setItem('groupCount',groupCount++)
+    getAllQuotes(groupCount)
   }
-  $("#tweet").attr("href", "https://twitter.com/intent/tweet?text=" + text);
+
+  localStorage.setItem('quteCount',quteCount++)
+  return quotes[quteCount].split('\n');
+}
+
+
+function getAllQuotes(groupNumber=1){
+  return fetch(api+groupNumber)
+  .then((response)=> {
+    return response.json();
+  })
+  .then((response)=> {
+    let newQuotesArray = response.map((quote)=>{
+      return (quote.quote  + quote.author + '//')
+    })
+    
+    localStorage.setItem('quotes',newQuotesArray)
+    localStorage.setItem('quteCount',0)
+    localStorage.setItem('groupCount',groupNumber)
+    return localStorage.getItem('quotes').split('//,');
+  });
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
   // nav menu
   const forms = document.querySelectorAll(".side-menu");
-  M.Sidenav.init(forms, { edge: "left" });
+  M.Sidenav.init(forms, { edge: "right" });
 
-  // var elems = document.querySelectorAll('.dropdown-trigger');
-  // var instances = M.Dropdown.init(elems);
+
 
   var elems = document.querySelectorAll("select");
   var instances = M.FormSelect.init(elems);
 
-  document.querySelector("#menu").addEventListener("click", () => {
-    document.querySelector("#card-quote").classList.remove("scale-out");
-    document.querySelector("#card-content-quote").classList.toggle("scale-out");
-    renderQuote((...args)=>{
-      console.log({...args});
+  let cardQuote=document.querySelector("#card-quote")
+  let cardContentQuote=cardQuote.querySelector("#card-content-quote");
+  let cardClickMenu=document.querySelector("#menu");
+
+
+  cardClickMenu.addEventListener("click",  function(){
+
+    let quoteData = getQuote();
+
+    cardQuote.classList.remove("scale-out");
+    cardContentQuote.classList.add("scale-out");
+    
+    cardContentQuote.addEventListener('transitionend',function() {
+      this.classList.add("scale-out");
+      this.classList.remove("scale-out");
+      cardContentQuote.removeEventListener('transitionend',()=>{})
+      cardContentQuote.children[0].textContent=quoteData[0];
+      document.querySelector('#content-text-author').textContent=quoteData[1];
+      
     })
+    this.children[0].style.animation='load-data-animation 1s';
   });
+
+  cardClickMenu.children[0].addEventListener('animationend',function() {
+    this.style.animation='';
+  });
+
 });
 
